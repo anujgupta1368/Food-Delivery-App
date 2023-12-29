1051e92af257360c5cd178e5c2b6421cd92e4472
anujgupta1368 <anujg1368@gmail.com>
2023-12-29 22:59:57 +0530

    commit

---
+++ backend/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://gofood:qwertyuiop@cluster0.grxau0b.mongodb.net/gofoodmern?retryWrites=true&w=majority'
+const mongoDB =async()=>{
+    await mongoose.connect(mongoURI,{useNewUrlParser: true}, async(err, result)=>{
+        if(err) console.log("---", err)
+        else{
+        console.log("connected");
+        const fecthed_data = await mongoose.connection.db.collection("food_items");
+        fecthed_data.find({}).toArray(async function( err, data){
+            const food_category = await mongoose.connection.db.collection("food_category");
+            food_category.find({}).toArray(function (err, catData){
+                if(err) console.log(err);
+                else {
+                    global.food_items = data;
+                    global.food_category = catData;
+                }
+            })
+            
+        })
+        }
+    });
+}
+
+module.exports = mongoDB;
+
+
